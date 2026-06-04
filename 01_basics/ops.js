4421a21f66fb932e8d7ef3e7faa434428d7b5068
Riturajhere <rituraj914024@gmail.com>
2026-06-04 18:29:05 +0000

    operations

---
+++ 01_basics/ops.js
@@ -0,0 +1,44 @@
+/*
+console.log(3+3);  //addition
+console.log(3-3);  //subtraction
+console.log(3*3);  //multiplication
+console.log(3/3);  //division
+console.log(3%2);  //modulus operator gives the remainder
+
+//increment and decrement operators
+
+let num= 10;
+num++;   //post increment operator, it will first return the value and then increment it by 1
+console.log(num);
+
+let num2= 10;
+++num2;  //pre increment operator, it will first increment the value by 1 and then return it
+console.log(num2);
+
+let num3= 10;
+num3--;  //post decrement operator, it will first return the value and then decrement it by 1
+console.log(num3);            
+
+let num4= 10;
+--num4;  //pre decrement operator, it will first decrement the value by 1 and then return it
+console.log(num4);
+
+*/
+
+
+/*
+console.log(3+3*3);  //order of operations, multiplication will be done first and then addition
+console.log((3+3)*3);  //parentheses will be evaluated first and then multiplication will be done   
+console.log("1" + "7")  //gives 17 because of string concatenation, if we want to add them as numbers we need to typecast them to numbers
+console.log(+"1" + +"7")  //gives 8 because of typecasting to numbers, the + operator before the string will convert it to a number
+
+console.log("1" + 7)  //gives 17 because of string concatenation, the number will be converted to a string and then concatenated    
+console.log(+"1" + 7)  //gives 8 because of typecasting to numbers, the + operator before the string will convert it to a number and then added to 7    
+console.log("1" + +"7")  //gives 17 because of string concatenation, the + operator before the string will convert it to a number and then added to 1 which is a string, so it will be converted to a string and then concatenated
+console.log(1+2+"4")  //gives 34 because of order of operations, the addition will be done first and then concatenation will be done, the result of addition will be converted to a string and then concatenated with "4" which is a string
+
+
+*/
+
+
+
