359f935cee2d3d6f7fa4ec66563814a2cc3aeb5f
Riturajhere <rituraj914024@gmail.com>
2026-06-05 10:55:31 +0000

    learn about writing objects, array and functions

---
+++ 01_basics/non_primitive.js
@@ -0,0 +1,43 @@
+let obj1 ={
+    name:"Rituraj",
+    age:21,
+    wt:81.12
+};
+
+
+// console.log(obj1);
+
+// or another way to define objects is 
+
+// const obj2= new Object();
+// obj2.name="Rituraj"
+// obj2.age=21
+// obj2.wt=81.12
+
+// console.log(obj2);
+
+
+// You can also define the objects as constructors and give them the values passed to the constructor function
+
+
+
+//arrays : 
+// const fruits = ["apple", "banana", "orange"];
+// const numbers = [1, 2, 3, 4, 5];
+// const mixed = [1, "hello", true, null];
+
+// console.table([fruits,numbers,mixed]);
+
+
+
+
+//functions: 
+
+// function greet(name) {
+//   return `Hello ${name}`;
+// }
+
+// const add = (a, b) => a + b;
+
+
+//That is all folks see you later
