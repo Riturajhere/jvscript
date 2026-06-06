bdc95be5529eb8c398ad8687ce927f66ae16598d
Riturajhere <rituraj914024@gmail.com>
2026-06-06 12:24:37 +0000

    STRING FUNCTIONS

---
+++ 01_basics/strings.js
@@ -0,0 +1,24 @@
+//placeholders :  ${}
+
+let name="Rituraj"
+let age= 19
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+//also called string interpolation
+
+//STRINGS ARE OBJECTS IN JS
+
+const str= new String("RiturajRS")
+
+console.log(str.toUpperCase());
+console.log(str[5]);
+console.log(name.toUpperCase());
+
+//Use slice or substring
+//slice can take negative values to reverse things
+const str2= name.slice(-1,3);
+console.log(str2);
+
+
+//THERE ARE MORE FUNCTIONS TO CHEKC OUT LATER
