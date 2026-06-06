6fe6bb3e9cf60691980a7d74967a5fe702fb6849
Riturajhere <rituraj914024@gmail.com>
2026-06-06 12:50:35 +0000

    Math functions in js

---
+++ 01_basics/Maths.js
@@ -0,0 +1,10 @@
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,87,999,34,23,534,1));
+
+
+console.log(Math.random());  // gives a value between 0 and 1
+
+console.log((Math.random()*10));
