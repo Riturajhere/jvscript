7cbadd6789998fab5e5171cd611ef240444e32a4
Riturajhere <rituraj914024@gmail.com>
2026-06-19 06:15:22 +0000

    forEach usage

---
+++ 03Functions/errors.js
@@ -0,0 +1,10 @@
+// try {
+//     // Code that might throw an error
+//     throw new Error('Something went wrong');
+// } catch (error) {
+//     // Handle the error
+//     console.error(error.message);
+// } finally {
+//     // Always executes, regardless of error
+//     console.log('Cleanup done');
+// }
--- 03Functions/forloops.js
+++ 03Functions/forloops.js
@@ -8,10 +8,21 @@ const person = {
 //FOR...in loop  
 
 for (const key in person) {
-    console.log(key, person[key]); // name Alice, age 30, city New York
+    console.log(key,`:`, person[key]); // name Alice, age 30, city New York
+}
+//*****FOR..In can be used to iterate through objects */
+
+
+const myobj={
+js:"javascript",
+rb:"ruby",
+swt:"Swift"
 }
 
 
+for(const key in myobj){
+    console.log((`${key} is a shortcut for ${myobj[key]}`));
+}
 
 //FOR...OF loop
 
@@ -32,3 +43,21 @@ const map = new Map([['a', 1], ['b', 2]]);
 for (const [key, value] of map) {
     console.log(key, value);
 }
+
+//**********objects are not iteratable like maps 
+
+
+
+
+
+
+//******FOR EACH  
+
+
+const coding=["js","ruby","c++","java"]
+
+coding.forEach(function(item){
+    console.log(item);
+    
+    //give any block of code inside this function , it will always take the array coding as the parameter
+})
